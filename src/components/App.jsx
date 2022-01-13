0dcb8889212a7c9756c13294e88b7a8dcaa34d40
DeyvidA <deyvida1001@gmail.com>
2022-01-13 11:14:40 -0600

    Configuracion initial y creacion d de las carpetas necesarias para la organizacion del proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
+import '../styles/app.css';
 
-const App = () => <h1>Hola Mundo</h1>
+const App = () => <h1>Hello </h1>;
 
 export default App;
