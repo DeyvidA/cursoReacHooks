3bc12b1beb80a40ea0065ba1adf8005144795f7e
DeyvidA <81656733+DeyvidA@users.noreply.github.com>
2022-02-28 12:04:39 -0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/components/Footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-      <p className="Footer-title">PlatziConf Merch</p>
+      <p className="Footer-title">DeyvidA Merch</p>
       <p className="Footer-copy">Todos los izquierdos reservados</p>
     </div>
   );
